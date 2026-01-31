e315de3e142683771cb4d88262b3a34c18f50cdd
kkrajid <kkrajid@gmail.com>
2026-01-31 12:40:02 +0530

    fix: add healthcheck.railway.app to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-cafe-iftar-dev-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.railway.app,.up.railway.app,healthcheck.railway.app,cafeiftarbackend-production.up.railway.app').split(',')
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
